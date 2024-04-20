7805a34b76cd3deb2660d0e93b19f9665dac539f
Sudo <Sudo248.dev@gmail.com>
2024-04-20 10:27:42 +0700

    fix ui

--- src/components/CommonWeather.jsx
+++ src/components/CommonWeather.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./CommonWeather.css";
-import {Flex} from "antd";
+import { Flex } from "antd";
 import WeatherInfo from "./WeatherInfo";
-import RightWeather from "./RightWeather";
+import SearchBar from "./SearchBar";
+import WeaklyWeather from "./WeaklyWeather";
 
 const CommonWeather = function (props) {
-  return (<Flex className="common-weather-wrapper" horizotal>
-    <WeatherInfo/>
-    <RightWeather/>
-  </Flex>);
+  return (
+    <div className="CommonWeather">
+      <WeatherInfo className="weather-info-wrapper" />
+      <SearchBar className="search-bar-wrapper"/>
+      <WeaklyWeather className="weather-weakly-wrapper"/>
+    </div>
+  );
 };
 
 export default CommonWeather;
